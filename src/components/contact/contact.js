c00a675409cc1e12791fe718ed6ab60599cf0c36
adamRohde <adamrohde@outlook.com>
2021-08-24 18:08:46 -0500

    li margin and made email a a tag

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -3,8 +3,11 @@ import React from "react";
 function contact() {
   return (
     <div className="contact-container">
-      <h3>Phone number - 612-242-5933</h3>
-      <h3>Email - cheshstyles@hotmail.com</h3>
+      <h3>Mobile - 612-242-5933</h3>
+      <h3>
+        Email -
+        <a href="cheshstyles@hotmail.com">cheshstyles@hotmail.com</a>
+      </h3>
       <h3>
         <a href="https://www.linkedin.com/in/rory-o-brien-1b1a2a1a4/">
           LinkedIn
